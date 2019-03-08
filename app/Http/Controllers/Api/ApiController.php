b0c984fd86be51cafff1fcbb65042d3f583ecae5
Christian <ChristianRogerpm@gmail.com>
2019-03-08 16:27:08 -0500

    okk

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -12,7 +12,8 @@ class ApiController extends Controller
 {
     public function EventoFecha(Request $request)
     {
-        $IdJuego = $request->input('IdJuego');
+//        $IdJuego = $request->input('IdJuego');
+        $IdJuego = 2;
         $evento_actual = Evento::EventoActual($IdJuego);
         $resultado_evento = ResultadoEvento::ResultadosEvento($IdJuego);
         if ($evento_actual == null) {
@@ -20,7 +21,9 @@ class ApiController extends Controller
         } else {
             $ganador = ResultadoEvento::ValorGanadorEvento($evento_actual->idEvento);
             $fecha_actual = $evento_actual->fechaEvento;
-            $fecha_evento_proximo = '';
+
+            $fecha_evento_proximo = new Carbon();
+
             if ($evento_actual->lapsoProxEventoHoras > 0) {
                 $NumeroHoras = $evento_actual->lapsoProxEventoHoras;
                 $fecha_evento_proximo = Carbon::parse($evento_actual->fechaEvento)->addHours($NumeroHoras);
@@ -31,11 +34,21 @@ class ApiController extends Controller
                 $NumeroMinutos = $evento_actual->lapsoProxEventoMinutos;
                 $fecha_evento_proximo = Carbon::parse($evento_actual->fechaEvento)->addMinutes($NumeroMinutos);
             }
+            $fecha_evento_prox = $fecha_evento_proximo->toDateTimeString();
+            $fecha_animacion = $fecha_evento_proximo->subSeconds($evento_actual->segBloqueoAntesEvento)->toDateTimeString();
+
+            $estado_animacion = false;
+
+            if (now() > $fecha_animacion && now() < $fecha_evento_prox) {
+                $estado_animacion = true;
+            }
             return response()->json([
                 'fecha_evento_actual' => $fecha_actual,
-                'fecha_evento_proximo' => $fecha_evento_proximo->toDateTimeString(),
-                'fecha_animacion' => $fecha_evento_proximo->subSeconds($evento_actual->segBloqueoAntesEvento)->toDateTimeString(),
+                'fecha_evento_proximo' => $fecha_evento_prox,
+                'fecha_animacion' => $fecha_animacion,
+                'evento_id_actual' => $evento_actual->idEvento,
                 'evento_valor_ganador' => $ganador->valorGanador,
+                'estado_animacion' => $estado_animacion,
                 'resultado_evento' => $resultado_evento
             ]);
         }
