002b57376f80097de853cdd5253d4bd048686e8e
Christian <ChristianRogerpm@gmail.com>
2019-04-17 15:07:19 -0500

    ok

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,7 @@ use Illuminate\Http\Request;
 //    return $request->user();
 //});
 
-Route::post('DataEventoResultadoEvento','Api\ApiController@EventoFecha');
+//Route::post('DataEventoResultadoEvento','Api\ApiController@EventoFecha');
 Route::post('ConfirmacionToken','Api\ApiController@ConfirmacionToken');
-Route::post('DataEventoResultadoEventoFk','Api\ApiController@EventoFecha_outValidate');
+//Route::post('DataEventoResultadoEventoFk','Api\ApiController@EventoFecha_outValidate');
+Route::post('DataEventoResultadoEventoFk','Api\ApiController@EventoFecha');
