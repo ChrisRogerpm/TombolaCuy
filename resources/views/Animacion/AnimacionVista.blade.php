56549c11739e7efbb219c4c27bace54c8b9a522a
r.blanco <r.blanco@software3000.net>
2019-04-19 18:14:39 -0500

    dash

--- resources/views/Animacion/AnimacionVista.blade.php
+++ resources/views/Animacion/AnimacionVista.blade.php
@@ -116,7 +116,7 @@
 	</div>	
 	<div id="pagosHistorial" class="nuevo_lados">
 		<div class="image" style="width: 80%;height: 170px;margin:auto;margin-top: 2vh;">
-			<img src="images/fondo_inicio.jpg"  alt="apuesta total" >		
+			<img src="images/figura_lado.jpg"  alt="apuesta total" >		
 		</div>
 		<h2>Pagos</h2>	
 
