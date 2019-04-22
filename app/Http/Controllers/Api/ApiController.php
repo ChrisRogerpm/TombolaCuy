de2ab9ae5ede15e43976ab62cb92e54ed016e1d0
Christian <ChristianRogerpm@gmail.com>
2019-04-22 13:19:17 -0500

    okk

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -30,7 +30,7 @@ class ApiController extends Controller
                 $fecha_evento_proximo = Carbon::parse($evento_actual->fechaEvento)->addMinutes($NumeroMinutos);
             }
             $fecha_evento_prox = $fecha_evento_proximo->toDateTimeString();
-            $segundos_agregados = $evento_actual->segBloqueoAntesEvento + 20;
+            $segundos_agregados = $evento_actual->segBloqueoAntesAnimacion;
             $fecha_animacion = Carbon::parse($fecha_evento_proximo)->subSeconds($segundos_agregados)->toDateTimeString();
             $token = Evento::ValidarTokenAnimacion($evento_actual->idEvento);
             $estado_animacion = false;
@@ -47,51 +47,13 @@ class ApiController extends Controller
                 'fecha_animacion' => $fecha_animacion,
                 'evento_id_actual' => $evento_actual->idEvento,
                 'evento_valor_ganador' => $ganador->valorGanador,
+                'segBloqueoAntesAnimacion' => $segundos_agregados
             ];
             return response()->json([
                 'evento' => $array_evento,
                 'resultado_evento' => $resultado_evento,
                 'estadistica' => $estadistica
             ]);
-
-//            if ($estado_animacion) {
-//                if ($evento_actual->estadoAnimacion == 0) {
-//                    $token = Evento::ValidarTokenAnimacion($evento_actual->idEvento);
-//                    $array_evento = [
-//                        'token_animacion' => $token,
-//                        'mensaje_token' => 'Esperando respuesta de token',
-//                        'estado_animacion' => $estado_animacion,
-//                        'fecha_evento_ini_actual' => $fecha_ini_actual,
-//                        'fecha_evento_fin_actual' => $fecha_fin_actual,
-//                        'fecha_evento_proximo' => $fecha_evento_prox,
-//                        'fecha_animacion' => $fecha_animacion,
-//                        'evento_id_actual' => $evento_actual->idEvento,
-//                        'evento_valor_ganador' => $ganador->valorGanador,
-//                    ];
-//                    return response()->json([
-//                        'evento' => $array_evento,
-//                        'resultado_evento' => $resultado_evento,
-//                        'estadistica' => $estadistica
-//                    ]);
-//                } else {
-//                    return response()->json(['mensaje' => 'El estado de animación del evento actual esta activado']);
-//                }
-//            } else {
-//                $array_evento = [
-//                    'fecha_evento_ini_actual' => $fecha_ini_actual,
-//                    'fecha_evento_fin_actual' => $fecha_fin_actual,
-//                    'fecha_evento_proximo' => $fecha_evento_prox,
-//                    'fecha_animacion' => $fecha_animacion,
-//                    'evento_id_actual' => $evento_actual->idEvento,
-//                    'evento_valor_ganador' => $ganador->valorGanador,
-//                ];
-//                return response()->json([
-//                    'mensaje' => 'No se encuentra en el tiempo de rango de Animación',
-//                    'evento' => $array_evento,
-//                    'resultado_evento' => $resultado_evento,
-//                    'estadistica' => $estadistica
-//                ]);
-//            }
         } else {
             $estadistica = TipoApuesta::EstadisticaUltimosTipoApuesta();
             $array_evento = [
@@ -104,6 +66,7 @@ class ApiController extends Controller
                 'fecha_animacion' => '',
                 'evento_id_actual' => '',
                 'evento_valor_ganador' => '',
+                'segBloqueoAntesAnimacion' => ''
             ];
             return response()->json([
                 'evento' => $array_evento,
